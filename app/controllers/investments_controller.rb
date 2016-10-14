c90f2e44f06da676612ab8c7e0cbb1ea900e7bb4
Eugene Kondrashov <eugene0707@gmail.com>
2016-10-14 08:18:02 +0000

    Stats refactored

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -11,33 +11,8 @@ class InvestmentsController < ApplicationController
   private
 
   def fetch_investment_stats
-    investment_stats = build_investment_stat
     investment = current_user.investments.first
-    begin
-      investment_stats.total_amount = investment.total_amount
-      investment_stats.term = investment.term
-      investment_stats.interest_rate = investment.interest_rate.to_f
-      investment_stats.expected_income = investment.expected_income
-      investment_stats.expected_profit = investment.expected_profit
-      investment_stats.actual_income = investment.actual_income
-      investment_stats.actual_rate = investment.actual_rate
-      investment_stats.actual_profit = investment.actual_profit
-    end if investment
-
-    gon.investment_stats = investment_stats.to_h
-  end
-
-  def build_investment_stat
-    investment_stats = OpenStruct.new
-    investment_stats.total_amount = 0
-    investment_stats.term = 0
-    investment_stats.interest_rate = 0
-    investment_stats.expected_income = 0
-    investment_stats.expected_profit = 0
-    investment_stats.actual_income = 0
-    investment_stats.actual_rate = 0
-    investment_stats.actual_profit = 0
-    investment_stats
+    gon.investment_stats = Investment.stats(investment)
   end
 
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -21,6 +21,20 @@ class Investment < ActiveRecord::Base
   validates_numericality_of :term, greater_than: 0
   validates_numericality_of :interest_rate, greater_than_or_equal_to: 0, less_than_or_equal_to: 1
 
+  def self.stats(instance = nil)
+    investment_stats = []
+    investment_stats[:total_amount] = instance.try(:total_amount) || 0
+    investment_stats[:term] = instance.try(:term) || 0
+    investment_stats[:interest_rate] = instance.try(:interest_rate).to_f || 0
+    investment_stats[:expected_income] = instance.try(:expected_income) || 0
+    investment_stats[:expected_profit] = instance.try(:expected_profit) || 0
+    investment_stats[:actual_income] = instance.try(:actual_income) || 0
+    investment_stats[:actual_rate] = instance.try(:actual_rate) || 0
+    investment_stats[:actual_profit] = instance.try(:actual_profit) || 0
+    investment_stats
+  end
+
+
   def total_amount
     @total_amount ||= loans.sum(:amount)
     @total_amount
